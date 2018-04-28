7e8ed32e3e2f8cb7a12b468ad97cdc58b9e6bc00
khanguslee <angus.lee809@gmail.com>
2018-04-28 15:25:29 +1000

    update message

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('Hello World');
-})
+});
 
 var server = app.listen(PORT, () => {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("Example app listening at ", port);
-})
+    console.log("Example app listening at ", PORT);
+});
